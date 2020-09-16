24b5af23d10c640af517e0ba34e29842d35386d7
Pratham <62739763+Pluffy2217@users.noreply.github.com>
2020-09-16 12:22:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,41 @@
+/*for theme switcher*/
+
 document.querySelector(".toggler").addEventListener('click',function() {
     document.body.classList.toggle("light");
+});
+
+/*for hover effects*/
+
+var follows1 = [".facebook",".twitter",".instagram",".youtube"];
+var follows2 = [];
+
+follows1.forEach(element => {
+    var y =  document.querySelector(element);
+    follows2.push(y);
+});
+
+follows2.forEach(element => {
+    element.addEventListener("mouseover",function() {
+        element.style.backgroundColor = "var(--hover-color)";
+    });
+    element.addEventListener("mouseout",function() {
+        element.style.backgroundColor = "var(--card-color)";
+    });
+});
+
+var overview1 = ['.b1','.b2','.b3','.b4','.b5','.b6','.b7','.b8'];
+var overview2 = [];
+
+overview1.forEach(element => {
+    var x = document.querySelector(element);
+    overview2.push(x);
+});
+
+overview2.forEach(element => {
+    element.addEventListener("mouseover",function() {
+        element.style.backgroundColor = "var(--hover-color)";
+    });
+    element.addEventListener("mouseout",function() {
+        element.style.backgroundColor = "var(--card-color)";
+    });
 });
